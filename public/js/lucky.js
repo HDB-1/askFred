f52d69e75f345e0d301edb1d511765e36b3518a7
nlaw96 <lawrencianjume@gmail.com>
2019-11-20 12:08:12 +0000

    Revert "Revert "added route to lucky.html and jquery logic""
    This reverts commit a8db6ede88323b647ecaa2decb1457d3bb51bed6.

--- public/js/lucky.js
+++ public/js/lucky.js
@@ -1,16 +1,3 @@
-window.onscroll = function() {myFunction()};
-
-var header = document.getElementById("myHeader");
-
-var sticky = header.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
 
 async function getSearchResults() {
     promise = await axios.get("http://localhost:8080/search.json");
@@ -18,51 +5,45 @@ async function getSearchResults() {
 }
 
 $(document).ready(() => {
-          
-             
+        
+       
+        
+            //event.preventDefault();
+            
             getSearchResults().then((response) =>  {
                 console.log(response.data.data)
-                return response.data.data;}).then((x) => {
-                    console.log(x)
+                return response.data.data;}).then((item) => {
+                    console.log(item.length)
                 let count = 1;
+                let index = Math.floor(Math.random()*10);
                 
-                x.forEach(element => {
                     $(".container-all-results")
                     .append(`<div class="result" id="result${count}"></div>`)
 
                     $(`#result${count}`)
                     .append(`<a class="title" id="title${count}"></a>`)
 
-                    $(`#title${count}`).attr("href", `${element.link}`);
+                    $(`#title${count}`).attr("href", `${item[index].link}`);
 
-                    $(`#title${count}`).text(`${element.title}`)
+                    $(`#title${count}`).text(`${item[index].title}`)
 
                     $(`#result${count}`)
                     .append(`<a class="link" id="link${count}"></a>`)
 
-                    $(`#link${count}`).attr("href", `${element.link}`);
+                    $(`#link${count}`).attr("href", `${item[index].link}`);
 
-                    $(`#link${count}`).text(`${element.link}`)
+                    $(`#link${count}`).text(`${item[index].link}`)
 
                     $(`#result${count}`)
                     .append(`<p class="snippet" id="snippet${count}"></p>`)
 
-                    $(`#snippet${count}`).text(`${element.snippet}`)
+                    $(`#snippet${count}`).text(`${item[index].snippet}`)
 
                     
 
-                    count++
-                });
+               
                 
             })
             .catch((error) => console.log(error.message))
-        
-  
-
-  
-
-
-    
-
 
 })
