9ef4265cefc7bf048de32dcbd0ea0bedcc97c83f
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2019-11-20 11:35:39 +0000

    Removing whitespaces and indenting code (script.js)

--- public/js/script.js
+++ public/js/script.js
@@ -12,74 +12,44 @@ function myFunction() {
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-//const axios = require('axios');
-
 async function getSearchResults() {
+    await axios.get("http://localhost:8080/results");
     promise = await axios.get("http://localhost:8080/search.json");
     return promise;
 }
 
 $(document).ready(() => {
-        
-       
-        
-            //event.preventDefault();
-            
-            getSearchResults().then((response) =>  {
-                console.log(response.data.data)
-                return response.data.data;}).then((x) => {
-                    console.log(x)
-                let count = 1;
-                
-                x.forEach(element => {
-                    $(".container-all-results")
-                    .append(`<div class="result" id="result${count}"></div>`)
+  getSearchResults().then((response) =>  {
+      console.log(response.data.data)
+      return response.data.data;}).then((x) => {
+          console.log(x)
+      let count = 1;
+      
+      x.forEach(element => {
+          $(".container-all-results")
+          .append(`<div class="result" id="result${count}"></div>`)
 
-                    $(`#result${count}`)
-                    .append(`<a class="title" id="title${count}"></a>`)
+          $(`#result${count}`)
+          .append(`<a class="title" id="title${count}"></a>`)
 
-                    $(`#title${count}`).attr("href", `${element.link}`);
+          $(`#title${count}`).attr("href", `${element.link}`);
 
-                    $(`#title${count}`).text(`${element.title}`)
+          $(`#title${count}`).text(`${element.title}`)
 
-                    $(`#result${count}`)
-                    .append(`<a class="link" id="link${count}"></a>`)
+          $(`#result${count}`)
+          .append(`<a class="link" id="link${count}"></a>`)
 
-                    $(`#link${count}`).attr("href", `${element.link}`);
+          $(`#link${count}`).attr("href", `${element.link}`);
 
-                    $(`#link${count}`).text(`${element.link}`)
+          $(`#link${count}`).text(`${element.link}`)
 
-                    $(`#result${count}`)
-                    .append(`<p class="snippet" id="snippet${count}"></p>`)
+          $(`#result${count}`)
+          .append(`<p class="snippet" id="snippet${count}"></p>`)
 
-                    $(`#snippet${count}`).text(`${element.snippet}`)
+          $(`#snippet${count}`).text(`${element.snippet}`)
 
-                    
-
-                    count++
-                });
-                
-            })
-            .catch((error) => console.log(error.message))
-
-  
-
-  
-
-
-    
-
-
+          count++
+      });
+  })
+  .catch((error) => console.log(error.message));
 })
