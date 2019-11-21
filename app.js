f542e98d414735d7819535f8587161d39abf43d4
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2019-11-21 15:29:52 +0000

    Search results (#32)
    * Beautifying app.js and writing additional comments
    
    * Bringing code to 1 line
    
    * Beautifying html
    
    * Updated api key and engine ID
    
    I also beautified the code
    
    * Update index.html
    
    * Update results.html
    
    * Update app.js
    
    * Adding comment
    
    * Changed to console.error(err)

--- app.js
+++ app.js
@@ -19,52 +19,55 @@ app.use(express.static(`${__dirname}/public`));
 // To extract form data from the POST request body
 app.use(express.urlencoded());
 
-// Finishing up the body-parser set up
-app.use(bodyParser.urlencoded({extended: true})); //find out more....
+// Parses bodies from URL
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Entering 'localhost:8080' into the url presents the homepage
 app.get('/', (req, res) => res.status(200).render('index'));
 
+// 
 app.post("/search", (req, res) => {
-    const query = JSON.stringify(req.body.search)
-    console.log(req.body.search);
-    console.log("converted query into string")
+    const query = JSON.stringify(req.body.search);
+    console.log(`What you searched for: ${req.body.search}.`);
+    console.log("Converted query into string.");
     googleAPI.search(query)
-    .then(res.sendFile(path.join(__dirname, "./search.json")))
-    .then( res.redirect("results"))
-    .catch((error) => console.log(error.message));
-    //.then(res.sendFile(path.join(__dirname, "./search.json")))
-    //.then(res.redirect("results"))
+        .then(res.sendFile(path.join(__dirname, "./search.json")))
+        .then(res.redirect("results"))
+        .catch((error) => console.log(error.message));
 });
 
+// Gets the data from the search.json file
 app.get("/data", (req, res) => {
-    const url = 'http://127.0.0.1:8080/search.json';
+    const url = 'http://localhost:8080/search.json';
     axios.get(url).then((response) => { 
         res.send(response.data)
     }).catch((error) => res.send(error.message))
 });
 
 app.get("/search.json", (req, res) => {
-    console.log("sent data to search.json");
-    res.sendFile(path.join(__dirname, "search.json"))});
+    console.log("Sent data to search.json");
+    res.sendFile(path.join(__dirname, "search.json"));
+});
 
-// This route presents the results page
+// Displays ten search results on the results page
 app.post("/results", (req, res) =>  {
     console.log("post request to render results page")
-    res.status(200).render('results')});
- // res.sendFile(path.join(__dirname, "results"));
+    res.status(200).render('results');
+});
 
+// This route presents the results page
 app.get("/results", (req, res) => {
-    console.log("get request to render results page")
+    console.log("GET request to render results page")
     res.sendFile(path.join(__dirname,"views/results.html"))});
 
 //  This route presents the mail page
 app.get("/mail", (req, res) => res.status(200).render('mail'));
-    // res.sendFile(path.join(__dirname, "mail"));
 
+//  This route presents the page that emerges as a result of pressing "Feeling Lucky" btn
 app.get("/lucky", (req, res) => {
-        console.log("get request to render lucky page")
-        res.sendFile(path.join(__dirname,"views/lucky.html"))});
+        console.log("GET request to render lucky page");
+        res.sendFile(path.join(__dirname,"views/lucky.html"));
+});
     
 // Listening to the server on port 8080
 app.listen(8080, '127.0.0.1', () => console.log('Listening to port 8080..'));
--- googleAPI.js
+++ googleAPI.js
@@ -5,38 +5,26 @@ const fs = require("fs");
 
 module.exports = {
     search: async (query = 'books') => {
-        const apiKey = 'AIzaSyA-TdPCCDqB8lR_4TQ2EvfN1mB7L0dX9rg';
-        const engineID = '008950057093096505639:dtlta9pfmwz';
-        const baseURL = `https://www.googleapis.com/customsearch/v1?key=${apiKey}&cx=${engineID}&q=${query}`;
-        console.log(baseURL)
-        console.log("created base URL")
+        const apiKey = 'AIzaSyCHjZJRHPy0x5-3Ju2P5pAvHIk-RCKDKTk';
+        const searchEngineID = '008950057093096505639:8rfl2l3nonh';
+        const baseURL = `https://www.googleapis.com/customsearch/v1?key=${apiKey}&cx=${searchEngineID}&q=${query}`;
+        console.log(`Base URL: ${baseURL}`);
         axios.get(baseURL).then(res => {
-            console.log("got response from axios")
-            return res.data.items;
-        }).then((items) => {
-            let json = {
-                'data': []
-            };
-            let i = 0;
-            items.forEach((item) => {
-                json.data[i] = {
-                    'title': `${item.title}`,
-                    'link': `${item.link}`,
-                    'snippet': `${item.snippet}`
-                }
-            i++;
-            })
-            console.log("created json")
-            return json;
-        }).then((json) =>{
-            fs.writeFile("./search.json", JSON.stringify(json, null, 4), (err) => {
-                if (err) {
-                    console.error(err);
-                    return;
-                };
-                console.log("File has been created");
-            });
-            }
-        ).catch((error) => console.log(error.message))     
+                console.log("Axios response");
+                return res.data.items;
+            }).then((items) => {
+                // Contains all items
+                let jsonListOfItems = {'data': []};
+                let i = 0;
+                items.forEach((item) => {
+                    // Individual item
+                    jsonListOfItems.data[i] = {'title': `${item.title}`, 'link': `${item.link}`, 'snippet': `${item.snippet}`}
+                    i++;
+                });
+                console.log("Created JSON object that contains list of items");
+                return jsonListOfItems;
+            // Creates the search.json file (which contains the jsonListOfItems)
+            }).then((json) => fs.writeFile("./search.json", JSON.stringify(json, null, 4), (err) => (err) ? console.error(err) : console.log("File has been created"))
+            ).catch((error) => console.log(`Error: ${error.message}`));     
     }
 }
