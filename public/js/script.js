2c6ee7c1988803487d913aef86f3a2f8f1d42225
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2019-11-21 15:43:17 +0000

    Beautifying code
    - ES5 to ES6 code.
    - Indented code appropriately.

--- public/js/script.js
+++ public/js/script.js
@@ -1,76 +1,67 @@
-window.onscroll = function() {myFunction()};
+window.onscroll = () => myFunction();
 
-var header = document.getElementById("myHeader");
+let header = document.getElementById("myHeader");
 
-var sticky = header.offsetTop;
+let sticky = header.offsetTop;
 
 const myFunction = () => (window.pageYOffset > sticky) ? header.classList.add("sticky") : header.classList.remove("sticky");
 
-async function getSearchResults() {
-    promise = await axios.get("http://localhost:8080/search.json");
-    return promise;
-}
+const getSearchResults = async () => await axios.get("http://localhost:8080/search.json");
 
 $(document).ready(() => {
-    
-  getSearchResults().then((response) =>  {
-      console.log(response.data.data)
-      return response.data.data;}).then((x) => {
-          console.log(x)
-      let count = 1;
-      
-      x.forEach(element => {
-          $(".container-all-results")
-          .append(`<div class="result" id="result${count}"></div>`)
 
-          $(`#result${count}`)
-          .append(`<a class="title" id="title${count}"></a>`)
+    getSearchResults().then((response) => {
+            console.log(response.data.data)
+            return response.data.data;
+        }).then((x) => {
+            console.log(x)
+            let count = 1;
 
-          $(`#title${count}`).attr("href", `${element.link}`);
+            x.forEach(element => {
+                $(".container-all-results")
+                    .append(`<div class="result" id="result${count}"></div>`)
 
-          $(`#title${count}`).text(`${element.title}`)
+                $(`#result${count}`)
+                    .append(`<a class="title" id="title${count}"></a>`)
 
-          $(`#result${count}`)
-          .append(`<a class="link" id="link${count}"></a>`)
+                $(`#title${count}`).attr("href", `${element.link}`);
 
-          $(`#link${count}`).attr("href", `${element.link}`);
+                $(`#title${count}`).text(`${element.title}`)
 
-          $(`#link${count}`).text(`${element.link}`)
+                $(`#result${count}`)
+                    .append(`<a class="link" id="link${count}"></a>`)
 
-          $(`#result${count}`)
-          .append(`<p class="snippet" id="snippet${count}"></p>`)
+                $(`#link${count}`).attr("href", `${element.link}`);
 
-          $(`#snippet${count}`).text(`${element.snippet}`)
+                $(`#link${count}`).text(`${element.link}`)
 
-          count++
-      });
-  })
-  .catch((error) => console.log(error.message));
+                $(`#result${count}`)
+                    .append(`<p class="snippet" id="snippet${count}"></p>`)
 
+                $(`#snippet${count}`).text(`${element.snippet}`)
 
-   
+                count++
+            });
+        })
+        .catch((error) => console.log(error.message));
 
+    var promise1 = new Promise(function (resolve, reject) {
+        setTimeout(function load() {
+            //Check if the current URL contains '#'
+            if (document.URL.indexOf("#") == -1) {
+                // Set the URL to whatever it was plus "#".
+                url = document.URL + "#";
+                location = "#";
 
+                //Reload the page
+                location.reload(true);
+            }
 
-var promise1 = new Promise(function(resolve, reject) {
-    setTimeout(function load() {
-         //Check if the current URL contains '#'
-   if(document.URL.indexOf("#")==-1)
-   {
-   // Set the URL to whatever it was plus "#".
-   url = document.URL+"#";
-   location = "#";
+            console.log("hey")
+            resolve('foo');
+        }, 1500);
+    });
 
-   //Reload the page
-   location.reload(true);
-   
-   }
-   console.log("hey")
+    promise1.then()
 
-      resolve('foo');
-    }, 1500);
-  });
-
-  promise1.then()
-
-})
+});
